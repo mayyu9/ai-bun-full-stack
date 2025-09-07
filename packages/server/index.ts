1506350a3e2f4664f98c5dbaf3f6a07b62dc528d
mayyu9 <coolmahimsd@gmail.com>
2025-09-07 14:51:42 +0530

    env file setup

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import type { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('hello world!!!!!!!!');
+    res.send(process.env.OPENAI_API_KEY);
 });
 
 app.listen(port, () => {
