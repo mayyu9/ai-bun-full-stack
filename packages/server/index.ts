281ae6df7487e57ea128b0ae044677ddb8ccbb28
mayyu9 <coolmahimsd@gmail.com>
2025-09-07 14:40:16 +0530

    basic server setup and project structure

---
+++ packages/server/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('hello world!!!!!!!!');
+});
+
+app.listen(port, () => {
+    console.log(`server is running on htttp:localhost: ${port}`);
+});
